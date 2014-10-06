3fc0a5688a21293a3e2a7ba58c8a828015243021
garndesh <garndesh@gmail.com>
2014-10-06 19:03:52 +0200

    added more evil

--- src/garndesh/csp/items/ItemCureBall.java
+++ src/garndesh/csp/items/ItemCureBall.java
@@ -1,5 +1,8 @@
 package garndesh.csp.items;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import garndesh.csp.CreeperSpores;
 import garndesh.csp.lib.Constants;
 import garndesh.csp.lib.Strings;
@@ -15,10 +18,39 @@ import net.minecraft.world.World;
 
 public class ItemCureBall extends ItemFood {
 
+	private PotionEffect hunger, weakness, poison, regen, digSlow, moveSlow, confusion, blindness, wither;
+	private int badness;
+	
 	public ItemCureBall() {
 		super(64, 0, false);
 		super.setAlwaysEdible();
 		setUnlocalizedName(Strings.RECOURCE_PREFIX+Strings.ITEM_CUREBALL_NAME);
+
+    	badness = Constants.CURE_BADNESS;
+    	float multiplyer = badness/3;
+		
+		hunger = new PotionEffect(Potion.hunger.id, (int) (2400*multiplyer), 0);
+        weakness = new PotionEffect(Potion.weakness.id, (int) (1200*multiplyer), 0);
+        poison = new PotionEffect(Potion.poison.id, (int) (600*multiplyer), 0);
+        regen = new PotionEffect(Potion.regeneration.id, (int) (600*multiplyer), 0);
+        digSlow = new PotionEffect(Potion.digSlowdown.id, (int) (600*multiplyer), 1);
+        moveSlow = new PotionEffect(Potion.moveSlowdown.id, (int) (600*multiplyer), 1);
+        confusion = new PotionEffect(Potion.confusion.id, (int) (600*multiplyer), 0);
+        blindness = new PotionEffect(Potion.blindness.id, (int) (600*multiplyer), 0);
+        wither = new PotionEffect(Potion.wither.id, (int) (600*multiplyer), 0);
+        
+        if(badness>=3){
+        	List<ItemStack> item = new ArrayList<ItemStack>();
+			hunger.setCurativeItems(item);
+	        weakness.setCurativeItems(item);
+	        poison.setCurativeItems(item);
+	        regen.setCurativeItems(item);
+	        digSlow.setCurativeItems(item);
+	        moveSlow.setCurativeItems(item);
+	        confusion.setCurativeItems(item);
+	        blindness.setCurativeItems(item);
+	        wither.setCurativeItems(item);
+        }
 	}
 
 	@Override
@@ -33,21 +65,20 @@ public class ItemCureBall extends ItemFood {
     {
         if (!world.isRemote)
         {
-        	int badness = Constants.CURE_BADNESS;
-        	float multiplyer = badness/3;
-            player.addPotionEffect(new PotionEffect(Potion.hunger.id, (int) (2400*multiplyer), 0));
-            player.addPotionEffect(new PotionEffect(Potion.weakness.id, (int) (1200*multiplyer), 0));
+            player.addPotionEffect(hunger);
+            player.addPotionEffect(weakness);
             if(badness>=3){
-	            player.addPotionEffect(new PotionEffect(Potion.poison.id, (int) (600*multiplyer), 0));
-	            player.addPotionEffect(new PotionEffect(Potion.regeneration.id, (int) (600*multiplyer), 0));
+	            player.addPotionEffect(poison);
+	            player.addPotionEffect(regen);
             }
             if(badness>=4){
-	            player.addPotionEffect(new PotionEffect(Potion.digSlowdown.id, (int) (600*multiplyer), 2));
-	            player.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, (int) (600*multiplyer), 2));
+	            player.addPotionEffect(digSlow);
+	            player.addPotionEffect(moveSlow);
             }
             if(badness>=5){
-	            player.addPotionEffect(new PotionEffect(Potion.confusion.id, (int) (600*multiplyer), 0));
-	            player.addPotionEffect(new PotionEffect(Potion.blindness.id, (int) (600*multiplyer), 0));
+	            player.addPotionEffect(confusion);
+	            player.addPotionEffect(blindness);
+	            player.addPotionEffect(wither);
             }
 	        player.getEntityData().setBoolean(Strings.INFECTED_TAG, false);
             CreeperSpores.instance.enforcer.removeInfectedPlayer(player);
